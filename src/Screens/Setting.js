407d05c46900de1545b32bc4c77ee002df11a32d
atif1998 <malatif39@ee.ceme.edu.pk>
2023-06-15 15:58:01 -0800

    topbar

--- src/Screens/Setting.js
+++ src/Screens/Setting.js
@@ -1,20 +1,43 @@
 //import liraries
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import {Button} from 'react-native-elements';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import ToggleSwitch from 'toggle-switch-react-native';
 
 // create a component
 const Setting = () => {
   return (
-    <View style={styles.container}>
-      <Text>Setting</Text>
+    <View>
+      <View style={styles.container}>
+        <View style={{backgroundColor: 'red'}}>
+          <Ionicons name="airplane-sharp" size={40} />
+        </View>
+        <View style={{backgroundColor: 'black'}}>
+          <Text style={{fontSize: 30}}>Airoplane mode</Text>
+        </View>
+        <View style={styles.input}>
+          <Ionicons name="wifi-outline" size={40} />
+        </View>
+      </View>
     </View>
   );
 };
 
 // define your styles
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flexDirection: 'row',
+    height: 40,
+  },
+  input: {
+    flex: 1,
+    backgroundColor: 'green',
+    alignItems: 'flex-end',
+  },
 });
 
 //make this component available to the app
 export default Setting;
+<ion-icon name="toggle-outline"></ion-icon>;
+<ion-icon name="wifi-outline"></ion-icon>;
--- src/components/Top.js
+++ src/components/Top.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import Setting from '../Screens/Setting';
 import HomeScreen from '../Screens/HomeScreen';
+import ThirdScreen from '../Screens/ThirdScreen';
 
 const Tab = createMaterialTopTabNavigator();
 
@@ -10,6 +11,7 @@ function Top() {
     <Tab.Navigator>
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Settings" component={Setting} />
+      <Tab.Screen name="info" component={ThirdScreen} />
     </Tab.Navigator>
   );
 }
