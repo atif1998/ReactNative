0239cf4cd16cea86110984a4b530e9e3eb5a3d8a
atif1998 <malatif39@ee.ceme.edu.pk>
2023-06-15 17:59:03 -0800

    route

--- src/Screens/Setting.js
+++ src/Screens/Setting.js
@@ -1,30 +1,31 @@
-//import liraries
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
-import {Button} from 'react-native-elements';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import ToggleSwitch from 'toggle-switch-react-native';
 
-// create a component
 const Setting = () => {
   return (
     <View>
+      <View style={{marginTop: 20, marginBottom: 20}}>
+        <Ionicons name="reorder-four-sharp" size={40} />
+      </View>
+
       <View style={styles.container}>
-        <View style={{backgroundColor: 'red'}}>
+        <View>
           <Ionicons name="airplane-sharp" size={40} />
         </View>
-        <View style={{backgroundColor: 'black'}}>
+        <View>
           <Text style={{fontSize: 30}}>Airoplane mode</Text>
         </View>
         <View style={styles.input}>
           <Ionicons name="wifi-outline" size={40} />
         </View>
       </View>
+      <Ionicons name="bluetooth-sharp" size={40} />
+      <Text style={{fontSize: 30}}>bluetooth</Text>
     </View>
   );
 };
 
-// define your styles
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
@@ -32,12 +33,9 @@ const styles = StyleSheet.create({
   },
   input: {
     flex: 1,
-    backgroundColor: 'green',
     alignItems: 'flex-end',
   },
 });
 
-//make this component available to the app
 export default Setting;
-<ion-icon name="toggle-outline"></ion-icon>;
-<ion-icon name="wifi-outline"></ion-icon>;
+<ion-icon name="menu-sharp"></ion-icon>;
