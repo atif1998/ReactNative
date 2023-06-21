b678a32ee5f98ee28e2741364895fe1754277b64
atif1998 <malatif39@ee.ceme.edu.pk>
2023-06-21 14:57:05 -0800

    screen

--- src/components/Header2.jsx
+++ src/components/Header2.jsx
@@ -1,31 +1,17 @@
 //import liraries
 import React, {Component} from 'react';
-import {View, Text, StyleSheet, Image} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
 // create a component
-const Book = () => {
+const Header2 = () => {
   return (
     <View>
       <View style={styles.container}>
         <Ionicons name="arrow-back-outline" size={30} />
-        <Text style={styles.text}>Book a room</Text>
+        <Text style={styles.text}>My Bookings</Text>
       </View>
-      <View style={{paddingHorizontal: 27}}>
-        <Image
-          style={{width: '100%', height: 167, marginTop: 20}}
-          source={require('../assests/images/Rectangle22803.png')}
-        />
-        <Text
-          style={{
-            color: '#7C3F92',
-            fontSize: 18,
-            fontFamily: 'DM Sans',
-            marginTop: 20,
-          }}>
-          Meeting room name
-        </Text>
-      </View>
+      <Text style={styles.text1}>Credits lefts: 24</Text>
     </View>
   );
 };
@@ -59,4 +45,4 @@ const styles = StyleSheet.create({
 });
 
 //make this component available to the app
-export default Book;
+export default Header2;
